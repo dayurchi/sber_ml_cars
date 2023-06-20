22bcd65be208a9e446e18a7729d26e662d578c42
Denis <diomir@mail.ru>
2023-06-20 18:02:39 +0300

    Added jsons

--- airflow/dags/predict.py
+++ airflow/dags/predict.py
@@ -1,28 +1,64 @@
 from datetime import datetime
 
+import os
+import json
 import pandas as pd
+import re
+from pandas import json_normalize
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 
 from ml_cars import predict
 
 
-INPUT_SESSIONS_PATH = '/opt/airflow/dags/sessions.csv'
-INPUT_HITS_PATH = '/opt/airflow/dags/hits.csv'
-OUTPUT_PREDICTION_PATH = '/opt/airflow/dags/prediction.csv'
-
+INPUT_JSON_PATH = '/opt/airflow/dags/new_jsons'
+OUTPUT_PREDICTION_PATH = '/opt/airflow/dags/results_jsons'
 
 def make_prediction():
+    def record_json(name):
+        new_filename = "prediction_new_" + record_path
 
-    print('Data load started ...')
-    sessions = pd.read_csv(INPUT_SESSIONS_PATH, index_col=0)
-    hits = pd.read_csv(INPUT_HITS_PATH, index_col=0)
-    print('Data load finished ...')
+        # Обработка разделителей пути с использованием os.path.join()
+        output_path = os.path.join(OUTPUT_PREDICTION_PATH, new_filename + ".json")
 
-    print('Prediction started ...')
-    prediction = predict(sessions, hits)
-    prediction.to_csv(OUTPUT_PREDICTION_PATH)
-    print('Prediction finished ...')
+        # Запись данных в новый файл
+        with open(output_path, 'w') as file:
+            file.write(name)
+
+    # Получение списка файлов в папке
+    file_list = os.listdir(INPUT_JSON_PATH)
+
+    for file_name in file_list:
+        # Путь к файлу
+        file_path = os.path.join(INPUT_JSON_PATH, file_name)
+
+        #Извлечение заголовка из названия файла
+        match = re.search(r'ga_hits_new_(\d{4}-\d{2}-\d{2})\.json', file_name)
+        if match:
+            record_path = match.group(1)
+
+            # Чтение JSON
+            with open(file_path) as json_file:
+                hits_data = json.load(json_file)
+
+            hits = json_normalize(hits_data, record_path=record_path)
+
+        match = re.search(r'ga_sessions_new_(\d{4}-\d{2}-\d{2})\.json', file_name)
+        if match:
+            record_path = match.group(1)
+
+            with open(file_path) as json_file:
+                sessions_data = json.load(json_file)
+
+            sessions = json_normalize(sessions_data, record_path=record_path)
+
+            if not sessions.empty and not hits.empty:
+                prediction = predict(sessions, hits)
+                prediction_json = prediction.to_json(orient='index')
+                record_json(prediction_json)
+            else:
+                prediction_json_empty = "{}"  # Пустой JSON
+                record_json(prediction_json_empty)
 
 
 dag = DAG(
--- airflow/dags/predict_db.py
+++ airflow/dags/predict_db.py
@@ -1,6 +1,10 @@
 from datetime import datetime
 
+import json
+import os
+import re
 import pandas as pd
+from pandas import json_normalize
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from sqlalchemy import create_engine, Table, MetaData
@@ -8,10 +12,8 @@ from sqlalchemy import create_engine, Table, MetaData
 from ml_cars import predict
 
 
-INPUT_SESSIONS_PATH = '/opt/airflow/dags/sessions.csv'
-INPUT_HITS_PATH = '/opt/airflow/dags/hits.csv'
-OUTPUT_PREDICTION_PATH = '/opt/airflow/dags/prediction.csv'
-
+INPUT_JSON_PATH = '/opt/airflow/dags/new_jsons'
+OUTPUT_PREDICTION_PATH = '/opt/airflow/dags/prediction_new.json'
 
 def save_prediction_to_db(
     session_id,
@@ -33,23 +35,41 @@ def save_prediction_to_db(
 
 def make_prediction():
 
-    print('Data load started ...')
-    sessions = pd.read_csv(INPUT_SESSIONS_PATH, index_col=0)
-    hits = pd.read_csv(INPUT_HITS_PATH, index_col=0)
-    print('Data load finished ...')
+    file_list = os.listdir(INPUT_JSON_PATH)
 
-    print('Prediction started ...')
-    date_time = datetime.now()
-    predictions = predict(sessions, hits)
-    print('Prediction finished ...')
+    for file_name in file_list:
+        # Путь к файлу
+        file_path = os.path.join(INPUT_JSON_PATH, file_name)
 
-    print('Creating a DB connection ...')
-    engine = create_engine('sqlite:////app/database.db')
+        # Извлечение заголовка из названия файла
+        match = re.search(r'ga_hits_new_(\d{4}-\d{2}-\d{2})\.json', file_name)
+        if match:
+            record_path = match.group(1)
 
-    print('Uploading into DB ...')
-    for session_id, prediction in predictions.to_dict().items():
-        save_prediction_to_db(session_id, prediction, date_time, engine, 'predictions')
-    print('Success!')
+            # Чтение JSON
+            with open(file_path) as json_file:
+                hits_data = json.load(json_file)
+
+            hits = json_normalize(hits_data, record_path=record_path)
+
+        match = re.search(r'ga_sessions_new_(\d{4}-\d{2}-\d{2})\.json', file_name)
+        if match:
+            record_path = match.group(1)
+
+            with open(file_path) as json_file:
+                sessions_data = json.load(json_file)
+
+            sessions = json_normalize(sessions_data, record_path=record_path)
+
+            if not sessions.empty and not hits.empty:
+                date_time = datetime.now()
+                predictions = predict(sessions, hits)
+                engine = create_engine('sqlite:////app/database.db')
+
+                print('Uploading into DB ...')
+                for session_id, prediction in predictions.to_dict().items():
+                    save_prediction_to_db(session_id, prediction, date_time, engine, 'predictions')
+                print('Success!')
 
 
 dag = DAG(
